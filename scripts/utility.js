320b92b769cc8be9f68f14993f2d22c7e3fc653b
sakib31p <sakibff820@gmail.com>
2024-02-14 23:04:35 +0600

    added all

--- scripts/utility.js
+++ scripts/utility.js
@@ -30,6 +30,12 @@ function setTextElementValueById(elementId, value){
     element.innerText = value;
 }
 
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
 function getARandomAlphabet(){
     // get or create an alphabets array 
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
