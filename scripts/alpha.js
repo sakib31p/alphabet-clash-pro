26f7e7715ee29ee1bf332d59a419276b2e95c8e2
sakib31p <sakibff820@gmail.com>
2024-02-14 22:54:14 +0600

    final game ready for play

--- scripts/alpha.js
+++ scripts/alpha.js
@@ -8,6 +8,53 @@
 //     // console.log(playgroundSection.classList);
 // }
 
+function handleKeyboardKeyupEvent(event){
+    const playerPress = event.key;
+    console.log('player press', playerPress);
+    // stop the game if pressed Esc
+    if(playerPress === 'Escape'){
+        gameOver();
+    }
+
+    // get expected to press 
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPress,expectedAlphabet);
+
+    // checked matched or not 
+    if(playerPress === expectedAlphabet){
+        console.log('you get a point');
+        // score update 
+        // 1 get the current score 
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        // 2 increase the score by 1 
+        const newScore = currentScore + 1;
+        // 3 show the update score 
+        currentScoreElement.innerText = newScore;
+
+
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('you lost a life');
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+        // step-2 deceise life by 1 
+        const newLife = currentLife - 1;
+        // step -3 show the update life 
+        currentLifeElement.innerText = newLife;
+        if(newLife === 0){
+            gameOver();
+        }
+    }
+}
+
+document.addEventListener('keyup', handleKeyboardKeyupEvent);
 
 function continueGame(){
     // step -1: generate alphabet game 
@@ -24,7 +71,25 @@ function continueGame(){
 }
 
 function play(){
+    // hide every thing show only playground 
     hideElementById('home-screen');
+    hideElementById('final-score')
     showElementById('play-ground');
+    // reset score and life 
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
     continueGame();
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+    // update final score 
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementValueById('last-score', lastScore);
+    // clear the last selected alphabet 
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet);
+
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -12,6 +12,23 @@ function setBackgroundColorById(elementId){
     element.classList.add('bg-orange-400');
 
 }
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+
+}
+
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
 
 function getARandomAlphabet(){
     // get or create an alphabets array 
